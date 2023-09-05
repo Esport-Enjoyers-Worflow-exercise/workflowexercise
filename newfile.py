9aaba2b39c37c843a8edfad50d3f0238f5131cda
stilianb <stilianb@gmail.com>
2023-09-05 18:23:20 +0000

    added new file called newfile.py

---
+++ newfile.py
@@ -0,0 +1,17 @@
+print("This is a new file")
+
+# Add lines from previous files
+print("I have changed this to the second file from contributor 2. treat me with care but faster")
+
+
+print("Hello World")
+
+print("This should work hopefully")
+print("AHHHH")
+
+
+number = 2
+
+if (number % 2 == 0):
+    print("this number is even")
+
